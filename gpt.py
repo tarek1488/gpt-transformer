3e2a70796bd969356607be394403d77124aefd2e
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-08-31 14:51:22 +0300

    adding the multi head attention class and normalizing the wei in the attention head

--- gpt.py
+++ gpt.py
@@ -10,9 +10,10 @@ block_size = 8
 class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
-        self.key_layer = nn.Linear(embed_dim, head_size, bias=False)
-        self.query_layer = nn.Linear(embed_dim, head_size)
-        self.value_layer = nn.Linear(embed_dim, head_size)
+        self.head_size = head_size
+        self.key_layer = nn.Linear(embed_dim, self.head_size, bias=False)
+        self.query_layer = nn.Linear(embed_dim, self.head_size)
+        self.value_layer = nn.Linear(embed_dim, self.head_size)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) #(T, T)
         self.drop_out = nn.Dropout(dropout)
     def forward(self, x):
@@ -24,11 +25,20 @@ class Head(nn.Module):
         value = self.query_layer(x) # (B, T, HEAD_SIZE)
         
         #calculating the attention scores
-        wei =   query @ key.permute(0,2,1)
+        wei =   query @ key.permute(0,2,1) * self.head_size ** 0.5
+        #the masked fill for decoder attention block 
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim =-1)
         wei = self.drop_out(wei)
-        
         #making the weight aggregation of values
         out = wei @ value # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_of_heads, head_size):
+        super().__init__()
+        #sub attention layers in each layer of out transformer network
+        self.heads([Head(head_size) for i  in range(num_of_heads)])
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1) #----------- B, T, num_of_heads × head_size
+            
